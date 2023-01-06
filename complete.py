83096d832dada3cc8e62982347afa80bc902bb41
Lander-Hatsune <wangwwx@yeah.net>
2023-01-07 01:52:37 +0800

    accels, save

--- complete.py
+++ complete.py
@@ -10,8 +10,9 @@ from queue import Queue
 import matplotlib.pyplot as plt
 from scipy.signal import convolve
 from scipy.ndimage import laplace
+from joblib import Parallel, delayed
 
-BORDER_RANGE = 20
+BORDER_RANGE = 80
 
 class MaskedImg:
     
@@ -84,7 +85,7 @@ class MaskedImg:
         candi_img = candi_img.astype(np.int64)
         
         X = self.border()
-        X2 = (X ** 2).sum()
+        # X2 = (X ** 2).sum()
         
         candi_img_pad = np.pad(candi_img, ((0,),
                                            (self.shape[0] - 1,),
@@ -103,8 +104,9 @@ class MaskedImg:
 
         time_start = time.time()
 
-        for cx, cy in np.ndindex(masksumZ2.shape):
-            assert(candi_img_pad[:, cx:, cy:].shape[1:] >= self.shape)
+        def _getdis(cx, cy):
+            cx, cy = cx * 10, cy * 10
+            # assert(candi_img_pad[:, cx:, cy:].shape[1:] >= self.shape)
             Z = self.border(candi_img_pad[:, cx:, cy:])
 
             XZ = (X * Z).sum() # 26.31s
@@ -121,15 +123,23 @@ class MaskedImg:
             # assert (Z2 == (Z ** 2).sum()), \
             #     f'Z2: {Z2}, Z2_brute: {(Z ** 2).sum()}'
 
-            dis = (X2 + Z2 - 2 * XZ)
+            # dis = (X2 + Z2 - 2 * XZ)
+            dis = Z2 - 2 * XZ
             # assert (dis == ((X - Z) ** 2).sum()), \
             #     f'dis: {dis}, dis_brute: {((X - Z) ** 2).sum()}'
 
             # dis = ((X - Z) ** 2).sum() # 26.97s
-            
-            if dis < min_dis:
-                min_dis = dis
-                chosen_pos = (cx, cy)
+            if (cx % 100 == 0 and cy == 0):
+                print(f'[{cx}/{masksumZ2.shape[0]}]')
+            return dis
+
+        dilated_shape = (masksumZ2.shape[0] // 10, masksumZ2.shape[1] // 10)
+        dismap = np.array(
+            Parallel(n_jobs=8)(delayed(_getdis)(cx, cy) \
+                                for cx, cy in np.ndindex(dilated_shape)))
+        chosen_pos = np.unravel_index(np.argmin(dismap), dilated_shape)
+        chosen_pos = chosen_pos[0] * 10, chosen_pos[1] * 10
+        print(chosen_pos)
 
         print(f'elapsed: {time.time() - time_start:.3f}s')
         return self.invmask(candi_img_pad[:, chosen_pos[0]:, chosen_pos[1]:]) + \
@@ -242,14 +252,20 @@ class MaskedImg:
     def blend(self, patch):
         print('blending')
 
-        result_n = self._img / 255.0
+        result_n = (patch.invmask(self._img) + patch.mask()) / 255.0
         patch_n = patch._img / 255.0
         mask = patch._mask
-        
-        for c in range(3):
-            for i in range(2048):
-                result_n[c] = result_n[c] + 0.25 * mask * \
-                    laplace(result_n[c] - patch_n[c])
+
+        time_start = time.time()
+        def _iter_channel(result_n, patch_n):
+            for i in range(4096):
+                result_n = result_n + 0.25 * mask * \
+                    laplace(result_n - patch_n)
+            return result_n
+        result_n = np.array(
+            Parallel(n_jobs=3)(delayed(_iter_channel)(result_n[c], patch_n[c]) \
+                               for c in range(3)))
+        print(f'elapsed: {time.time() - time_start:.3f}s')
         return result_n.clip(0, 1) * 255
 
 if __name__ == '__main__':
@@ -261,8 +277,8 @@ if __name__ == '__main__':
 
     candi_dir = sys.argv[3]
 
-    Image.fromarray(src.border().transpose((1, 2, 0))
-                    .astype(np.uint8)).show()
+    # Image.fromarray(src.border().transpose((1, 2, 0))
+    #                 .astype(np.uint8)).show()
     
     for candi_name in os.listdir(candi_dir):
         print(f'Candidate image: {candi_name}')
@@ -270,17 +286,20 @@ if __name__ == '__main__':
         candi_img = np.array(Image.open(candi_path))[..., :3].transpose((2, 0, 1))
 
         patch_img = src.match(candi_img)
-        Image.fromarray(patch_img.transpose((1, 2, 0))
-                        .astype(np.uint8)).show()
+        # Image.fromarray(patch_img.transpose((1, 2, 0))
+        #                 .astype(np.uint8)).show()
 
         patch_mask = np.logical_or(src.cut_patch(patch_img), (1 - src_mask))
-        Image.fromarray((patch_mask * 255).astype(np.uint8)).show()
+        # Image.fromarray((patch_mask * 255).astype(np.uint8)).show()
         
         patch = MaskedImg(patch_img, patch_mask)
-        Image.fromarray((patch.mask() + patch.invmask(src_img))
-                        .transpose((1, 2, 0)).astype(np.uint8)).show()
+        # Image.fromarray((patch.mask() + patch.invmask(src_img))
+        #                 .transpose((1, 2, 0)).astype(np.uint8)).show()
 
         result = src.blend(patch)
-        Image.fromarray(result.transpose((1, 2, 0)).astype(np.uint8)).show()
-        
+        # Image.fromarray(result.transpose((1, 2, 0)).astype(np.uint8)).show()
+
+        os.system(f'mkdir -p results/{candi_dir}')
+        (Image.fromarray(result.transpose((1, 2, 0)).astype(np.uint8))
+         .save(f'results/{candi_path}'))
     
